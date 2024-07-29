d1433256ef7b580ee101047ed8763907969e4860
h-waldi575 <h-waldi@outlook.de>
2024-07-29 13:58:00 +0200

    Add files via upload

---
+++ kreismenu/script.js
@@ -0,0 +1,34 @@
+let menus=[...document.querySelectorAll('.circular-menu')];
+menus.map((menu)=>{
+	let items=menu.querySelectorAll('.circular-menu_item');
+	let button=menu.querySelector('.circular-menu_button');
+	let active=false;
+
+	const length=items.length;
+	const arc=2*Math.PI*(1/length);
+	const radius=40;
+
+	button.addEventListener('click', (e)=>{
+		e.preventDefault();
+
+		active= !active;
+		if (active) {
+			button.classList.add('circular-menu_button_active');
+
+			for(let i=0; i < length; i++) {
+				const angle=i*arc;
+				const x=radius*Math.cos(angle);
+				const y=radius*Math.sin(angle);
+
+				items[i].style.left=50+x+'%';
+				items[i].style.top=50+y+'%';
+			}
+
+		} else{
+			button.classList.remove('circular-menu_button_active');
+			for(let i=0; i<length; i++){
+				items[i].removeAttribute('style');
+			}
+		}
+	});
+});
